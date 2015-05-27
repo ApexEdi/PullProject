a0360c9a7e720e0eaeb28424150e506d9b09747e
apexedi <apexpatel.edi@gmail.com>
2015-05-27 16:46:05 +0530

    New Class Test

---
+++ App_Code/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for TestClass
+/// </summary>
+public class TestClass
+{
+	public TestClass()
+	{
+
+	}
+
+    public string EmpID { get; set; }
+    public string EmpName { get; set; }
+}
